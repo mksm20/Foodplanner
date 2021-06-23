a329ad7ed00cba3dbf6585bbfbecde213b1de461
mksm20 <mksm20@student.aau.dk>
2021-06-23 13:38:08 +0200

    basic functionality added

---
+++ node-api-call/api.js
@@ -0,0 +1,17 @@
+function callMealDB(searchParam) {
+  const request = require('request');
+
+  const options = {
+    url:
+      'http://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata' +
+      searchParam,
+    headers: { Authorization: 'token=1' }, // Demo API KEY
+  };
+
+  request.get(options, function (error, response, body) {
+    const info = JSON.parse(body);
+    console.log(info);
+  });
+}
+
+module.exports = { callMealDB };
